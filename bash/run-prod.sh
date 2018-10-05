2bf0776c1a3162594bd8d225c42ef6e7e215f733
Gabriel Augendre <gabriel@augendre.info>
2018-10-05 18:32:57 +0200

    Fix run script

--- bash/run-prod.sh
+++ bash/run-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 yes yes | pipenv run python manage.py migrate && \
 pipenv run python manage.py collectstatic --noinput && \
-pipenv run gunicorn workout.wsgi -b 0.0.0.0:8000 --log-file -
+pipenv run gunicorn fake_news_detector_api.wsgi -b 0.0.0.0:8000 --log-file -
