3d203762a752da16ef01115dd7371ad025dc54fc
Gabriel Augendre <gabriel@augendre.info>
2018-10-06 11:34:46 +0200

    Return 400 with explicit error message when no URL instead of 500

--- api/views.py
+++ api/views.py
@@ -6,9 +6,17 @@ from api.models import WebPage
 def web_page_score_view(request):
     web_page_url = request.GET.get('url')
     if not web_page_url:
-        return None
+        return JsonResponse({
+            'status': 'error',
+            'data': {
+                'message': 'No URL provided'
+            }
+        }, status=400)
 
-    return JsonResponse(WebPage.from_url(url=web_page_url).to_dict())
+    return JsonResponse({
+        'status': 'success',
+        'data': WebPage.from_url(url=web_page_url).to_dict()
+    })
 
 
 def ping_view(request):
