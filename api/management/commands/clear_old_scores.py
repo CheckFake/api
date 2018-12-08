d1ed1aa4adaf4b39591d1e33637cdaca7e9c84d2
Gabriel Augendre <gabriel@augendre.info>
2018-12-08 16:49:06 +0100

    Clear old scores when booting

---
+++ api/management/commands/clear_old_scores.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+from api.models import WebPage
+
+
+class Command(BaseCommand):
+    help = 'Deletes pages with old content score'
+
+    def handle(self, *args, **options):
+        res = WebPage.objects.filter(scores_version__lt=WebPage.CURRENT_SCORES_VERSION).delete()
+        self.stdout.write(self.style.SUCCESS(f'Successfully deleted pages with old content score {res}'))
--- bash/run-prod.sh
+++ bash/run-prod.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 yes yes | pipenv run python manage.py migrate && \
+pipenv run python manage.py clear_old_scores && \
 pipenv run python manage.py clear_empty_scores && \
 pipenv run python manage.py collectstatic --noinput && \
 LOAD_NLP="True" pipenv run gunicorn fake_news_detector_api.wsgi -b 0.0.0.0:8000 -t 600 -k gthread --threads 4 --log-file -
